eb77f43f3c1a80a9fc16e278ed4dabfc6dc038ea
dulakshirb <dulakshirb@gmail.com>
2024-10-05 16:02:37 +0530

    Create Discover page UI 70%

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color(0XFFF2F7F4);
+  static Color secondaryColor = const Color(0XFF1D1D1D);
+  static Color backgroundColor = const Color.fromARGB(162, 215, 215, 215);
+}
